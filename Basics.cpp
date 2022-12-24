6858b7b0293fb8aef7bfc4d5a3af36cf7351f204
Hitanshu Gupta <72181617+HitanshuGupta@users.noreply.github.com>
2022-12-24 17:46:10 +0530

    #Day 1 Activities

---
+++ Basics.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //   cout<< 3*7/2<<endl;
+    //   cout<< 7/2*3;
+
+    // overfloww
+    // int,char,long int, long long int, float, double
+
+    // -10^9< int <10^9
+    // -10^12 < long int < 10^12
+    // -10^18 < long long int < 10^18
+
+    // double a = 100000;
+    // double b = 100000;
+    // double c = a * b;
+    // c = 1e24;
+    // cout<<fixed<<c<<endl;
+
+    int l, b;
+    cin >> l >> b;
+    cout << l *1LL* b << endl;
+}
